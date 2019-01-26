b6d7d6a76d91ca719348639d54be181f76e2fd63
Petrauskas Titas <PG006273>
2019-01-26 12:32:57 +0200

    Windows Update 2020 Winter Update

---
+++ Project1/NASA/devil.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+#include<devil.h>
+using namespace std;
+
+int main() {
+	cout << "a#gZGj2Kb5paE4aTzgpD!?W@aZ@#h?Bw+w#$Vw3pjsrd9-yg6G6UmUKaks9!&2gr+dZ9yQ-PeZBmtK7?kG88+U#8Byb$kUq$FJt6mz_8rdwqCFrczf6GE9Y8f_Affx " << endl;
+}
